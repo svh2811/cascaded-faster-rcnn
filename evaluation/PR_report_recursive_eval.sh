5d31dd711ac9ce7f81376606ac077904db65e688
srideepika <j.srideepika@gmail.com>
2018-10-28 19:10:16 -0400

    fixed gypsum scripts

--- evaluation/PR_report_recursive_eval.sh
+++ evaluation/PR_report_recursive_eval.sh
@@ -6,8 +6,9 @@ do
 	rm ./vis_images/*
 	cp -v ../character-faster-rcnn/DataGeneration/maps_red/* ./vis_images/
 #	python ./visualize_maps.py -r ../../map_res/words-det-fold_0.88_${i}.txt -t ../data_generation/fold_1/test.txt
-	python ./visualize_maps.py -r ../character-faster-rcnn/map_res/words-det-fold-01.txt -t ../DataGeneration/test.txt
+	python ./visualize_maps.py -r ../character-faster-rcnn/map_res/words-det2-D0090-5242001-01.txt -t ../character-faster-rcnn/DataGeneration/test_d.txt
         #python ./generate_IoU_report.py -r ../character-faster-rcnn/map_res/words-det-fold-01.txt -t ../character-faster-rcnn/DataGeneration/test.txt
+	rm -r ./results_CNF_NMS_NPYS/
 	mkdir ./results_CNF_NMS_NPYs
 	mkdir ./results_CNF_NMS_NPYs/CNF0.88_NMS${i}
 	cp -v ./vis_images/*.npy ./results_CNF_NMS_NPYs/CNF0.88_NMS${i}
