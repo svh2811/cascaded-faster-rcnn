e185eae3370491b76fba596c3626235a3af2a0d9
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-11-01 21:18:37 -0400

    buf fix

--- evaluation/visualize_maps_grid_search.py
+++ evaluation/visualize_maps_grid_search.py
@@ -26,6 +26,8 @@ import config
 
 for nms_thresh in config.NMS_THRESH_LIST:
     for conf_tresh in config.CONF_THRESH_LIST:
+        hyper_params = str(nms_thresh) + "_" + str(conf_tresh)
+        print(hyper_params)
         prediction_file =  prediction_folder + "/words-det2-" + hyper_params + "_" +  config.FID_EXTENSION + "-D0090-5242001-01.txt"
         print(prediction_file)
 
