5c4c3c3810871505cfa3750ad08854234b332e2b
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-11-06 17:39:01 -0500

    Paint angled rectangles

--- character-faster-rcnn/phoc-extension-heatmap.py
+++ character-faster-rcnn/phoc-extension-heatmap.py
@@ -14,7 +14,7 @@ for name in glob.glob("/home/supadhyay/supadhyay/dicts/D0090-5242001.npy"):
 			continue
 		char_mask = np.zeros((H, W), dtype=np.uint8)
 		for bbox in map.item()[key]:
-			tl, br = bbox[0], bbox[1]
-			char_mask[tl[1]:br[1]+1, tl[0]:br[0]+1] = 255
+			# TODO: get those points
+			cv2.fillPoly(char_mask, [np.int32([tl, tr, br, bl])], 255)
 		np.save("./phoc-extension-heatmap/" + imgName + "-" + key + ".npy", char_mask)
 		print("File written: " + imgName + "-" + key)
