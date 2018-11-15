3c2a22b988cc55bbf41dfed32dff80a45638a562
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-11-15 14:41:02 -0500

    update to create mask for all chars

--- character-faster-rcnn/phoc-extension-heatmap.py
+++ character-faster-rcnn/phoc-extension-heatmap.py
@@ -2,19 +2,21 @@ import glob
 import numpy as np
 import cv2
 
-for name in glob.glob("/home/supadhyay/supadhyay/dicts/D0090-5242001.npy"):
+all_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for name in glob.glob("/home/svh/cascaded-faster-rcnn/character-faster-rcnn/dicts_test/*.npy"):
 	map = np.load(name)
-	imgName = name[name.rfind('/')+1 : -4]
-	# print("ImageName: " + imgName)
+	#imgName = name[name.rfind('/')+1 : -4]
+	print("ImageName: " + imgName)
 	img = cv2.imread("./DataGeneration/maps/" + imgName + ".tiff")
 	H, W, C = img.shape
 	img = None
-	for key in map.item().keys():
-		if key != 'y':
-			continue
+	missed_chars = map.item().keys();
+	for char in all_chars:
 		char_mask = np.zeros((H, W), dtype=np.uint8)
-		for bbox in map.item()[key]:
-			# TODO: get those points
-			cv2.fillPoly(char_mask, [np.int32([tl, tr, br, bl])], 255)
-		np.save("./phoc-extension-heatmap/" + imgName + "-" + key + ".npy", char_mask)
+		if char in missed_chars: 
+			for bbox in map.item()[key]:
+				# TODO: get those points
+				cv2.fillPoly(char_mask, [np.int32([tl, tr, br, bl])], 255)
+		np.save("./stage-01/" + imgName + "-" + key + ".npy", char_mask)
 		print("File written: " + imgName + "-" + key)
